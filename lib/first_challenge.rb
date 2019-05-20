ee8e77c62f9bfd9a6dafc53a5f574c4df86f38dd
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-20 06:01:35 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,11 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts["Freddy Mercury"].collect do |attribute, value|
+    if (attribute == "favorite_icecream_flavors")
+      contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if{|flavor| flavor == "strawberry"}
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
