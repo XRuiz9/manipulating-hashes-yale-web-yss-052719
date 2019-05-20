f098cf6ffc5648555f0ac6b80ed6d41d8ab268ea
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-20 06:15:01 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,8 @@ def first_challenge
     }
   }
 
-  contacts.collect do |person, data|
-    data.collect do |attribute, value|
+  contacts.each do |person, data|
+    data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         if value.include?("strawberry")
           value.delete("strawberry")
